cdd93fbb465ee4257e27603f55ea6e850dd239a6
manojkumarmanikonda <mmanoj77@in.ibm.com>
2025-03-25 03:03:59 -0400

    Db Connect

--- AppoinmentManger/src/main/java/com/appoinmentManger/AppoinmentMangerApplication.java
+++ AppoinmentManger/src/main/java/com/appoinmentManger/AppoinmentMangerApplication.java
@@ -1,4 +1,4 @@
-package com.appoinmentManger.AppoinmentManger;
+package com.appoinmentManger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
