81cf1f974becd3155ed815fb330bcb9038c8cf44
manojkumarmanikonda <mmanoj77@in.ibm.com>
2025-03-25 00:06:54 -0400

    intially comment

---
+++ AppoinmentManger/src/main/java/com/appoinmentManger/AppoinmentManger/AppoinmentMangerApplication.java
@@ -0,0 +1,13 @@
+package com.appoinmentManger.AppoinmentManger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppoinmentMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppoinmentMangerApplication.class, args);
+	}
+
+}
