779074b99a419b0a3e9bd158a9f41d8a2fe59a6b
manojkumarmanikonda <mmanoj77@in.ibm.com>
2025-03-26 02:03:47 -0400

    Cross issue

--- AppoinmentManger/src/main/java/com/appoinmentManger/controller/UserController.java
+++ AppoinmentManger/src/main/java/com/appoinmentManger/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/bookings")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private UserService userService;
